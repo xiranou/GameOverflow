811958ceca15edca5d495c6fcfd5b70f78912364
Rebecca Paige Perez <rebeccapaige1216@gmail.com>
2014-11-20 18:16:05 -0500

    Added invalid title and content tests

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,4 +3,14 @@ FactoryGirl.define do
     title "I LUV GAMES"
     content "THEY ARE COOL!!!!!1!1!1!111"
   end
+
+  factory :notitle_article do
+    title
+    content "THEY ARE COOL!!!!!1!1!1!111"
+  end
+
+  factory :nocontent_article do
+    title "I LUV GAMES"
+    content
+  end
 end
--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -5,6 +5,12 @@ describe Article do
     article = create(:article)
     expect(article).to be_valid
   end
-  it "should be invalid if it does not have a title."
-  it "should be invalid if it does not have content."
+  it "should be invalid if it does not have a title." do
+    article = create(:notitle_article)
+    expect(article).to be_invalid
+  end
+  it "should be invalid if it does not have content." do
+    article = create(:nocontent_article)
+    expect(article).to be_invalid
+  end
 end

fcc3d505bf9c595f4b73354914a2fec3316c0fa0
xiranou <xiranou@gmail.com>
2014-11-20 19:43:55 -0500

    refactored rspec test with Shoulda Gem

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 describe Comment do
   describe "testing Comment validation" do
-    it "should be invalid without text" do
-      comment = build(:comment, text: nil)
-      expect(comment).to be_invalid
-    end
-
-    it "should be valid with text" do
-      comment = build(:comment)
-      expect(comment).to be_valid
-    end
+    it {should validate_presence_of(:text)}
   end
 end

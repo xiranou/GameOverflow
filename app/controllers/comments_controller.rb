e5460f6cb30bb60ea01f91f08edf9d9527a41a5a
xiranou <xiranou@gmail.com>
2014-11-21 12:05:48 -0500

    add create method to controller and strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,4 +16,19 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to comment_path(@comment)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end

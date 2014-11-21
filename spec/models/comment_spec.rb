54fdaf734dca6e3539570878a4338c0ea029a7d1
xiranou <xiranou@gmail.com>
2014-11-20 19:37:39 -0500

    fix specs for comment invalidation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe Comment do
   describe "testing Comment validation" do
     it "should be invalid without text" do
-      comment = create(:invalid_comment)
+      comment = build(:comment, text: nil)
       expect(comment).to be_invalid
     end
 
     it "should be valid with text" do
-      comment = create(:comment)
+      comment = build(:comment)
       expect(comment).to be_valid
     end
   end

c4c7c61ecbe73e146eacb75897047c38852c7459
xiranou <xiranou@gmail.com>
2014-11-21 16:28:15 -0500

    delete unuse factories

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,11 +3,4 @@ FactoryGirl.define do
     title "I LUV GAMES"
     content "THEY ARE COOL!!!!!1!1!1!111"
   end
-
-  factory :invalid_article do
-    title nil
-    content "THEY ARE COOL!!!!!1!1!1!111"
-  end
-
-
 end

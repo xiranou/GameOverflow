9d7094597f4fcd6c76d1cfa1c099dc377150b878
xiranou <xiranou@gmail.com>
2014-11-22 12:45:02 -0500

    fix spelling in specs

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Vote do
   xit { should belong_to :voter }
-  xit { should validate_presences_of :voter_id }
+  xit { should validate_presence_of :voter_id }
   it { should belong_to :voteable }
-  it { should validate_presences_of :voteable }
+  it { should validate_presence_of :voteable }
 end

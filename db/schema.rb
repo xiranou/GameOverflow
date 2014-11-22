809afef4dd612772571794216faa6e135badd521
xiranou <xiranou@gmail.com>
2014-11-21 21:59:50 -0500

    add migration for adding parent_id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121024008) do
+ActiveRecord::Schema.define(version: 20141122025823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141121024008) do
     t.integer  "commenter_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id"
   end
 
 end

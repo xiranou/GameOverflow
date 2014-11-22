ea435cdca47c5898eaa567550a44108b19c17c47
aj9045 <identity_unveiled@hotmail.com>
2014-11-22 17:07:48 -0500

    add missing 'do'

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Game do
-  describe "testing Game validations"
+  describe "testing Game validations" do
     it {should validate_presence_of(:title)}
     it {should belong_to(:genre)}
     it {should belong_to(:console)}

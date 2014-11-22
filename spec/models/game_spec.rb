6a8294671fdda72c1bcd3dd9f76cf2d70cef27ce
aj9045 <identity_unveiled@hotmail.com>
2014-11-22 17:05:46 -0500

    add game model spec

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Game do
+  describe "testing Game validations"
+    it {should validate_presence_of(:title)}
+    it {should belong_to(:genre)}
+    it {should belong_to(:console)}
+  end
+end

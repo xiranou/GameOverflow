d27e7f0e63b15304a1d1c1f3a90916276c217365
xiranou <xiranou@gmail.com>
2014-11-22 12:58:37 -0500

    add has_many votes for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,6 @@ class Comment < ActiveRecord::Base
 
   belongs_to :parent, class_name: "Comment"
   has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
+
+  has_many :votes, as: :voteable
 end

128f88ac1181e390f47c0f7913934b136fe6e22f
xiranou <xiranou@gmail.com>
2014-11-21 22:03:14 -0500

    add self-join association for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   validates_presence_of :text
 
   belongs_to :article
+
+  belongs_to :parent, class_name: "Comment"
+  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
 end

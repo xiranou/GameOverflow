788d52ac2933178b0323b90bfb66d35542481443
xiranou <xiranou@gmail.com>
2014-11-22 12:43:49 -0500

    update vote model specs

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe Vote do
-  it { should belong_to :voter }
-  it { should validate_presences_of :voter_id }
+  xit { should belong_to :voter }
+  xit { should validate_presences_of :voter_id }
   it { should belong_to :voteable }
+  it { should validate_presences_of :voteable }
 end

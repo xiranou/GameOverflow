8b7e73e390bbab70cb0a304ce7696b787b07e4f1
Jensen Bouzi <jensen.bouzi@gmail.com>
2014-11-22 16:05:40 -0500

    add spec for polymorphic association and ability to have comments

--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -9,4 +9,10 @@ describe Article do
   it { should validate_presence_of :title }
 
   it { should validate_presence_of :content }
+
+  it { should have_many(:comments)}
+
+  it {should belong_to(:discussable)}
+
+  it {should }
 end

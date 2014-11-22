33752513f8dd4a78aeaef9b458444e86ee289d93
xiranou <xiranou@gmail.com>
2014-11-21 21:58:02 -0500

    update model specs for Comment to test self-join association

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe Comment do
   describe "testing Comment validation" do
     it {should validate_presence_of(:text)}
+    it {should belong_to(:parent).class_name("Comment")}
+    it {should has_many(:replies).dependent(:destroy)}
   end
 end

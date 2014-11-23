d45b678af81f69af5fcddab5a14e6db568328aab
xiranou <xiranou@gmail.com>
2014-11-23 18:37:52 -0500

    fix bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    render template: "articles/index", locals:{articles: @articles}
+    render template: "articles/show", locals:{articles: @articles}
   end
 
   def edit

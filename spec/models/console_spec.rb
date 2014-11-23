fce8a44e1fbd733d48f4371c8e4ec92a3621caed
aj9045 <identity_unveiled@hotmail.com>
2014-11-23 11:49:57 -0500

    correct and add do to console spec

--- spec/models/console_spec.rb
+++ spec/models/console_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Console do
-  describe "testing Console validations"
+  describe "testing Console validations" do
     it {should validate_presence_of(:name)}
     it {should have_many(:games)}
   end

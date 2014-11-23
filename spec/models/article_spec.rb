432274a47c3343c04773acf9271f8e8945f8d66e
Jensen Bouzi <jensen.bouzi@gmail.com>
2014-11-23 14:08:12 -0500

    deleted unnecessary tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 describe Article do
   describe "validations" do
-    it "has a title and content." do
-      article = create(:article)
-      expect(article).to be_valid
-    end
     it { should validate_presence_of :title }
     it { should validate_presence_of :content }
     it { should validate_presence_of :author_id}

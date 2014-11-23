6f684f117aff829c83fffe6a87c95cf49f3023dd
xiranou <xiranou@gmail.com>
2014-11-23 15:37:39 -0500

    update vote_spec to validate voter_id in scope of voteable_id

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,6 +4,7 @@ describe Vote do
   describe "validations" do
     it { should validate_presence_of :voter_id }
     it { should validate_presence_of :voteable }
+    it { should validate_uniqueness_of(:voter_id).scoped_to(:voteable_id) }
   end
   describe "associations" do
     it { should belong_to(:voter).class_name("User") }

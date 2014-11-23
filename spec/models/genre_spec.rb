0773660356248ae4df883c6d391627a2c06925b0
aj9045 <identity_unveiled@hotmail.com>
2014-11-23 12:58:22 -0500

    correct the do again

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Game do
-  describe "testing Game validations"
+  describe "testing Game validations" do
     it {should validate_presence_of(:name)}
     it {should have_many(:games)}
   end

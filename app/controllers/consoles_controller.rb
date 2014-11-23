64959d14ed0988ffbd1020edef61932219f40a66
aj9045 <identity_unveiled@hotmail.com>
2014-11-23 13:51:38 -0500

    add consoles controller

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,3 +1,48 @@
 class ConsolesController < ApplicationController
 
+  def index
+    @consoles = Console.all
+  end
+
+  def show
+    @console = Console.find(params[:id])
+  end
+
+  def edit
+    @console = Console.find(params[:id])
+  end
+
+  def update
+    @console = Console.find(params[:id])
+
+    if @console.update_attributes(console_params)
+      redirect_to console_path(@console)
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @console = Console.new
+  end
+
+  def create
+    @console = Console.new(console_params)
+
+    if @console.save
+      redirect_to console_path(@console)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def console_params
+    params.require(:console).permit(:name)
+  end
+
+
+
 end

90058b5b9dccc86d0c0ffc1f5b7d599ac5219727
aj9045 <identity_unveiled@hotmail.com>
2014-11-23 14:09:58 -0500

    fill in genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,46 @@
 class GenresController < ApplicationController
 
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+
+    if @genre.update_attributes(genre_params)
+      redirect_to genre_path(@genre)
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+
+    if @genre.save
+      redirect_to genre_path(@genre)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end

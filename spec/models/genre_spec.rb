a04c98d29ae31a51d4de4f7722b28903f6c2df18
aj9045 <identity_unveiled@hotmail.com>
2014-11-23 14:00:46 -0500

    add genre factory

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe Game do
-  describe "testing Game validations" do
+describe Genre do
+  describe "testing Genre validations" do
     it {should validate_presence_of(:name)}
     it {should have_many(:games)}
   end

63df2a3758020a4184365374dc42a8540f4a2c95
xiranou <xiranou@gmail.com>
2014-11-23 17:04:42 -0500

    update seed file with discussables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
 require 'faker'
 
-user = User.create(handle: "test", email: "testing@gmail.com", password: "test")
+user = User.create(handle: "test", email: "test@gmail.com", password: "test")
+
+["xbox-one","ps4","wii-u"].each do |console|
+  Console.create(name: console)
+end
+
+consoles = Console.all
+
+consoles.each do |console|
+  console.games.create(title: Faker::App.name, genre: Genre.create(name: Faker::Hacker.verb))
+end
+
+discussables = Console.all + Game.all + Genre.all
 
 10.times do
-  Article.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph, author: user)
+  Article.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph, author: user, discussable: discussables.sample)
 end
 
 Article.all.each do |article|

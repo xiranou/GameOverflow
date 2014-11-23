a1f5eb974bc49e27d371c17709085d7cc93bef76
xiranou <xiranou@gmail.com>
2014-11-23 16:41:57 -0500

    add association spec for console/game/genre

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -4,5 +4,6 @@ describe Genre do
   describe "testing Genre validations" do
     it {should validate_presence_of(:name)}
     it {should have_many(:games)}
+    it { should have_many(:articles) }
   end
 end

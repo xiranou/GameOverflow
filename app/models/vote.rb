527db0fca2de36b9223c2e44642cde0c0f38c08a
xiranou <xiranou@gmail.com>
2014-11-23 12:40:09 -0500

    update validation for votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
-  validates_presence_of :voteable
+  validates_presence_of :voteable, :voter_id
   belongs_to :voteable, polymorphic: true
   belongs_to :voter, class_name: "User"
 end

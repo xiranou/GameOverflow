b3dff66a732e280856e634314c16ef806c50b95e
aj9045 <identity_unveiled@hotmail.com>
2014-11-23 23:47:31 -0500

    update seed file..use this one

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,36 @@
 require 'faker'
 
-User.create(handle: "test", email: "test@gmail.com", password: "test")
+User.create(handle: "Faker::Name.first_name", email: "test@gmail.com", password: "test")
+
 user = User.first
 
-["xbox-one","ps4","wii-u"].each do |console|
+
+
+games = ["Left 4 Dead", "Assassin's Creed: Revelations", "Battlefield 4", "Far Cry 3", "Castlevania", "Wolf Among Us", "BioShock", "Dark Souls", "Fallout 3", "Devil May Cry"]
+
+games.each do |game|
+  Game.find_or_create_by(title: game)
+end
+
+genres = ["RPG", "Shooter", "Sport", "Action", "Adventure", "Arcade", "Fighting", "Racing", "Strategy", "Puzzle & Cards", "Other"]
+
+genres.each do |genre|
+  Genre.find_or_create_by(name: genre)
+end
+
+consoles = ["PS3", "PS4", "Wii U", "Xbox One", "Xbox 360", "PC", "Vita", "3DS", "Mobile"]
+
+consoles.each do |console|
   Console.find_or_create_by(name: console)
 end
 
-consoles = Console.all
 
-consoles.each do |console|
-  console.games.create(title: Faker::App.name, genre: Genre.create(name: Faker::Hacker.verb))
+
+all_games = Game.all
+
+all_games.each do |game|
+  game.update(console: Console.find(rand(1..Console.count)))
+  game.update(genre: Genre.find(rand(1..Genre.count)))
 end
 
 discussables = Console.all + Game.all + Genre.all
@@ -20,35 +40,11 @@ end
 
 topics = Topic.all
 20.times do
-  Article.create!(title: Faker::Lorem.word, content: Faker::Lorem.paragraph, author: user, topics: topics.sample(2))
+  Article.create!(title: Faker::Commerce.product_name, content: Faker::Lorem.paragraph, author: user, topics: topics.sample(2))
 end
 
 Article.all.each do |article|
   article.comments.create(text: Faker::Lorem.paragraph, commenter: user)
+end
 
-
-Game.create(title: "Left 4 Dead")
-Game.create(title: "Assassin's Creed: Revelations")
-
-Genre.create(name: "RPG")
-Genre.create(name: "Shooter")
-Genre.create(name: "Sport")
-Genre.create(name: "Action")
-Genre.create(name: "Adventure")
-Genre.create(name: "Arcade")
-Genre.create(name: "Fighting")
-Genre.create(name: "Racing")
-Genre.create(name: "Strategy")
-Genre.create(name: "Puzzle & Cards")
-Genre.create(name: "Other")
-
-Console.create(name: "PS4")
-Console.create(name: "PS3")
-Console.create(name: "Wii U")
-Console.create(name: "Xbox One")
-Console.create(name: "Xbox 360")
-Console.create(name: "PC")
-Console.create(name: "Mobile")
-Console.create(name: "Vita")
-Console.create(name: "3DS")
 

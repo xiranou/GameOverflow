02caaaf7418ee228ce417f5e8a05dd8e57817201
beccapaige <rebeccapaige1216@gmail.com>
2014-11-23 20:12:05 -0500

    Added vote count column to articles table to allow for sorting by popular posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123205654) do
+ActiveRecord::Schema.define(version: 20141124010908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141123205654) do
     t.integer  "author_id"
     t.integer  "voteable_id"
     t.string   "voteable_type"
+    t.integer  "vote_count"
   end
 
   create_table "comments", force: true do |t|

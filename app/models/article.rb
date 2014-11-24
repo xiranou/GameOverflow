5b0762421fa7cd44418aa5ff0cd30f8bb7291b8e
xiranou <xiranou@gmail.com>
2014-11-23 20:02:14 -0500

    update aritcle model

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ActiveRecord::Base
   belongs_to :author, class_name: "User"
 
   has_many :topics
-  has_many :genre_topic, through: :topics, source_type: "Genre"
-  has_many :game_topic, through: :topics, source_type: "Game"
-  has_many :console_topic, through: :topics, source_type: "Console"
+  has_one :genre_topic, through: :topics, source_type: "Genre"
+  has_one :game_topic, through: :topics, source_type: "Game"
+  has_one :console_topic, through: :topics, source_type: "Console"
 end

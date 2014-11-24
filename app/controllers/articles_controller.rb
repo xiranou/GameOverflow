67fbcb6f9981a6c82a4ce26e4c628ae44d126288
beccapaige <rebeccapaige1216@gmail.com>
2014-11-23 19:38:33 -0500

    Was working in wrong file, moved methods to model.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -77,18 +77,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def content_preview
-
-  end
-
-  def recent
-
-  end
-
-  def popular
-
-  end
-
   private
 
   def article_params
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,16 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :votes, as: :voteable
   belongs_to :author, class_name: "User"
+
+  def content_preview
+
+  end
+
+  def recent
+
+  end
+
+  def popular
+
+  end
 end

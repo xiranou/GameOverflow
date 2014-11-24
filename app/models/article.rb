53639055a94d813f6d8ba12f048e17a2f6d207b8
beccapaige <rebeccapaige1216@gmail.com>
2014-11-23 19:43:10 -0500

    Working content_preview method, changed recent and popular to class methods

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,14 +11,14 @@ class Article < ActiveRecord::Base
   has_one :console_topic, through: :topics, source_type: "Console"
 
   def content_preview
+  	  self.content.split("")[1..20].join.concat("...")
+  end
+
+  def self.recent
 
   end
 
-  def recent
-
-  end
-
-  def popular
+  def self.popular
 
   end
 

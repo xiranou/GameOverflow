f38a89cfd89ee9628c5b2f9e0b7e0c48d36309e3
xiranou <xiranou@gmail.com>
2014-11-23 19:33:14 -0500

    update topcis table with article_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124003047) do
+ActiveRecord::Schema.define(version: 20141124003242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20141124003047) do
   create_table "topics", force: true do |t|
     t.integer "discussable_id"
     t.string  "discussable_type"
+    t.integer "article_id"
   end
 
   create_table "users", force: true do |t|
